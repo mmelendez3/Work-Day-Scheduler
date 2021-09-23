250a415cec19cd73f7db2f7bdde284a51968da4f
mmelendez3 <mele0019@gmail.com>
2021-09-23 08:21:32 -0500

    add date and time

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+var time = moment().format('LLLL')
+
+const currentDate = $('#currentDay')
+currentDate.text(time)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var saveTasks = function() {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+//   };
+  
+// $(".container").on("click", "button", function(){
+//     //console.log($(this)) WORKS
+    
+//     //targets "textarea" for event listener
+//     var text = $("textarea")
+//     .val()
+//     .trim()
+//     //console.log(text) WORKS
+  
+//   })
+
+// saveTasks()
